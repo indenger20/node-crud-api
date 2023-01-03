921f111a341d99740e84cdafbf01f8c22fd68282
yaroslav.sklema <yaroslav.sklema@gmail.com>
2023-01-04 00:51:22 +0200

    removed unused code

--- src/components/users/user.controller.ts
+++ src/components/users/user.controller.ts
@@ -3,17 +3,11 @@ import HttpStatusCodes from '../../shared/constants/httpStatusCodes';
 import { RequestResult } from '../../shared/httpServer/types';
 import UserService from './user.service';
 
-const loop = () =>
-  new Promise((resolve) => {
-    setTimeout(() => resolve(1), 300);
-  });
-
 class UserController {
   static async findAll(): Promise<RequestResult> {
     // const result = await UserService.findAll();
 
     console.log('cluster.worker?.id', cluster.worker?.id);
-    await loop();
     return {
       actionResult: [],
       statusCode: HttpStatusCodes.OK,
@@ -21,7 +15,6 @@ class UserController {
   }
 
   static async create(): Promise<RequestResult> {
-    console.log('cluster.worker?.id', cluster.worker?.id);
     return {
       actionResult: [],
       statusCode: HttpStatusCodes.OK,
